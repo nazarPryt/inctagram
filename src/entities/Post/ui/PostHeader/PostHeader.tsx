cf0b34a28cfd50472f0d3b2c0dcd4bbdd88048c1
Tatsiana1111 <tanyashket@gmail.com>
2023-07-26 10:58:56 +0300

    feat: add Popover component

--- src/entities/Post/ui/PostHeader/PostHeader.tsx
+++ src/entities/Post/ui/PostHeader/PostHeader.tsx
@@ -1,30 +1,70 @@
-import React from 'react'
 import {PostHeaderWrapper} from 'entities/Post/ui/PostHeader/PostHeader.styled'
-import {PopOverIcon} from 'entities/Post/ui/PostHeader/popOverIcon'
-import {IconButton} from 'shared/components/IconButton/IconButton'
 import {AvatarIcon} from 'shared/components/AvatarIcon/AvatarIcon'
+import CopyOutline from '../../../../common/assets/icons/copyOutline.png'
+import RemovePerson from '../../../../common/assets/icons/personRemove.png'
+import EmailOutline from '../../../../common/assets/icons/emailOutline.png'
 import Link from 'next/link'
 import {PATH} from 'shared/constants/PATH'
+import {useState} from 'react'
+import {Popover} from '../../../../shared/components/Popover/Popover'
+import {IconButton} from '../../../../shared/components/IconButton/IconButton'
+import Image from 'next/image'
 
 type PostHeaderType = {
     img: string
     userID: number
 }
+
 export const PostHeader = ({img, userID}: PostHeaderType) => {
     const BASE_URL = process.env.NEXT_PUBLIC_NEXTAUTH_URL as string
+
+    const [isPopoverOpen, setIsPopoverOpen] = useState(false)
+    const handleActionOne = () => {
+        console.log('Action One')
+        setIsPopoverOpen(false)
+    }
+    const handleActionTwo = () => {
+        console.log('Action Two')
+        setIsPopoverOpen(false)
+    }
+    const handleActionThree = () => {
+        console.log('Action Three')
+        setIsPopoverOpen(false)
+    }
 
     return (
         <PostHeaderWrapper>
             <div>
                 <AvatarIcon img={img} userID={userID} />
                 <Link className={'link'} href={`${BASE_URL}${PATH.USER_PROFILE}/${userID}`}>
-                    URLProfiele
+                    URLProfile
                 </Link>
                 <span>22 Minutes ago</span>
             </div>
-            <IconButton>
-                <PopOverIcon />
-            </IconButton>
+            <Popover
+                onActionHandler={handleActionOne}
+                setIsPopoverOpen={setIsPopoverOpen}
+                isPopoverOpen={isPopoverOpen}
+            >
+                <div id='iconWrapper'>
+                    <IconButton onClick={handleActionOne}>
+                        <Image style={{width: '24px', height: '24px'}} src={EmailOutline} alt={'email icon'} />
+                    </IconButton>
+                    <span>Report</span>
+                </div>
+                <div id='iconWrapper'>
+                    <IconButton onClick={handleActionTwo}>
+                        <Image style={{width: '24px', height: '24px'}} src={RemovePerson} alt={'email icon'} />
+                    </IconButton>
+                    <span>Unfollow</span>
+                </div>
+                <div id='iconWrapper'>
+                    <IconButton onClick={handleActionThree}>
+                        <Image style={{width: '24px', height: '24px'}} src={CopyOutline} alt={'email icon'} />
+                    </IconButton>
+                    <span>Copy</span>
+                </div>
+            </Popover>
         </PostHeaderWrapper>
     )
 }
