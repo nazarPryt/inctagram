c6bf84e2c1199bd2154abc5d9a61273351c25b63
Igor Shargin <igor20513@gmail.com>
2023-07-26 19:48:43 +0500

    feat: added base fonts

---
+++ src/_app/themes/typography.ts
@@ -0,0 +1,22 @@
+export const typography = {
+    fontFamilyPrimary: 'Inter',
+
+    // line heights
+    lineHeightS: '16px',
+    lineHeightM: '24px',
+    lineHeightL: '36px',
+
+    // font sizes
+    fontSizeXS: '0.75rem',
+    fontSizeS: '0.875rem',
+    fontSizeM: '1rem',
+    fontSizeL: '1.125rem',
+    fontSizeXL: '1.25rem',
+    fontSizeXXL: '1.625rem',
+
+    // font weights
+    fontWeightRegular: 400,
+    fontWeightMedium: 500,
+    fontWeightSemiBold: 600,
+    fontWeightBold: 700,
+}
