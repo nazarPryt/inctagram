a0f970205e66374659f97415b6c9637dcd872629
Tatsiana1111 <tanyashket@gmail.com>
2023-08-26 21:57:06 +0300

    fix: BurgerMenu

--- src/widgets/Header/Header.tsx
+++ src/widgets/Header/Header.tsx
@@ -4,21 +4,21 @@ import Link from 'next/link'
 import {ThemeSwitcher} from 'features/ThemeSwitcher/ThemeSwitcher'
 import {PATH} from 'shared/constants/PATH'
 import {LangSelect} from 'shared/ui/LangaugeSelect/LangSelect'
-import {BurgerModal} from 'features/Burger/ui/BurgerModal/BurgerModal'
-import {Notification} from '../../features/Notification/ui/Notification/Notification'
 import {BurgerMenu} from 'features/Burger/ui/BurgerMenu/BurgerMenu'
+import {Notification} from 'features/Notification/ui/Notification/Notification'
 
 export const Header = (props: ComponentPropsWithoutRef<'div'>) => {
     return (
-        <HeaderStyled  {...props}>
+        <HeaderStyled {...props}>
             <BurgerMenu className={'BurgerMenu'} />
-                <BurgerModal />
-                <Link className={'InctagramLogo'} href={PATH.HOME}>
-                    Inctagram
-                </Link>
-                <ThemeSwitcher />
+            <Link className={'InctagramLogo'} href={PATH.HOME}>
+                Inctagram
+            </Link>
+            <ThemeSwitcher />
+            <div className='block'>
                 <Notification />
                 <LangSelect />
+            </div>
         </HeaderStyled>
     )
 }
