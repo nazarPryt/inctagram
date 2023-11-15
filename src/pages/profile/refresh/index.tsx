a07468e7f5cd3d467da6eada6e27ea75aff9924c
nazarPtyt <ockap90@libero.it>
2023-11-15 15:23:53 +0100

    feat: created customAxios

--- src/pages/profile/refresh/index.tsx
+++ src/pages/profile/refresh/index.tsx
@@ -6,22 +6,20 @@ import axios from 'axios'
 import {serverAuthAPI} from 'shared/server-api/server-api'
 
 export const getServerSideProps: GetServerSideProps = async (ctx: GetServerSidePropsContext) => {
-    nookies.set(ctx, 'refreshToken', 'new', {path: '/', httpOnly: true, secure: true})
+    // nookies.set(ctx, 'refreshToken', 'new', {path: '/', httpOnly: true, secure: true})
+    //
+    // const BASE_URL = process.env.NEXT_PUBLIC_BASE_URL
+    // const cookies = nookies.get(ctx)
+    // const accessToken = cookies.accessToken
+    //
+    // if (accessToken)
+    try {
+        const res = await serverAuthAPI.authMe(ctx)
 
-    const BASE_URL = process.env.NEXT_PUBLIC_BASE_URL
-    const cookies = nookies.get(ctx)
-    const accessToken = cookies.accessToken
-
-    if (accessToken)
-        try {
-            const res = await serverAuthAPI.authMe(accessToken)
-
-            return {props: {me: res}}
-        } catch (e) {
-            return {props: {me: JSON.stringify(e)}}
-        }
-
-    return {props: {me: 'return'}}
+        return {props: {me: res}}
+    } catch (e) {
+        return {props: {me: JSON.stringify(e)}}
+    }
 }
 
 export default function RefreshPage(props: any) {
--- src/shared/server-api/server-api.ts
+++ src/shared/server-api/server-api.ts
@@ -1,71 +1,74 @@
 import axios from 'axios'
-import cookie from 'react-cookies'
-import {accessToken} from 'shared/constants/constants'
+import {accessToken, refreshToken} from 'shared/constants/constants'
+import {GetServerSidePropsContext} from 'next'
+import nookies from 'nookies'
 
-const baseURL = process.env.NEXT_PUBLIC_BASE_URL as string
+//https://gist.github.com/xstevenyung/560c880992b3ad6892923cbad582bd81  <-- Axios Instance Example
 // const domainURL = process.env.NEXT_PUBLIC_DOMAIN_URL as string
 
-export const instance = axios.create({
-    withCredentials: true,
-    baseURL,
-})
+export const customAxios = (ctx: GetServerSidePropsContext) => {
+    const baseURL = process.env.NEXT_PUBLIC_BASE_URL as string
 
-instance.interceptors.request.use(
-    config => {
-        const token = cookie.load(accessToken)
+    const instance = axios.create({
+        withCredentials: true,
+        baseURL,
+    })
 
-        if (token) {
-            config.headers.Authorization = 'Bearer ' + token
-        }
+    instance.interceptors.request.use(
+        config => {
+            const cookies = nookies.get(ctx)
+            const accessToken = cookies.accessToken
 
-        return config
-    },
-    error => Promise.reject(error)
-)
-
-instance.interceptors.response.use(
-    config => {
-        return config
-    },
-    async error => {
-        const originalRequest = error.config
-
-        if (error.response.status == 401 && error.config && !error.config._isRetry) {
-            originalRequest._isRetry = true
-            try {
-                await serverAuthAPI.refreshTokens()
-
-                return instance.request(originalRequest)
-            } catch (e) {
-                console.log('User is not authorized (in interceptors.response)')
+            if (accessToken) {
+                config.headers.Authorization = 'Bearer ' + accessToken
             }
+
+            return config
+        },
+        error => Promise.reject(error)
+    )
+
+    instance.interceptors.response.use(
+        config => {
+            return config
+        },
+        async error => {
+            const originalRequest = error.config
+
+            if (error.response.status == 401 && error.config && !error.config._isRetry) {
+                originalRequest._isRetry = true
+                try {
+                    const res = await serverAuthAPI.refreshTokens(ctx)
+
+                    return instance.request(originalRequest)
+                } catch (e) {
+                    console.log('User is not authorized (in interceptors.response)')
+                }
+            }
+            throw error
         }
-        throw error
-    }
-)
+    )
+    return instance
+}
 
 export const serverAuthAPI = {
-    async authMe(token: string) {
+    async authMe(ctx: GetServerSidePropsContext) {
         try {
-            const res = await axios.get<authMeDataType>(`${baseURL}auth/me`, {
-                withCredentials: true,
-                headers: {
-                    Authorization: `Bearer ${token}`,
-                },
-            })
+            const res = await customAxios(ctx).get<authMeDataType>(`auth/me`)
 
             return res.data
         } catch (e) {
             throw new Error('Cant make authMe request')
         }
     },
-    async refreshTokens() {
+    async refreshTokens(ctx: GetServerSidePropsContext) {
         try {
-            const res = await instance.post<{accessToken: string}>(`${baseURL}auth/update-tokens`, {
-                withCredentials: true,
-            })
+            const res = await customAxios(ctx).post<{accessToken: string}>(`auth/update-tokens`)
 
-            cookie.save(accessToken, res.data.accessToken, {})
+            console.log('res', res)
+
+            nookies.set(ctx, accessToken, res.data.accessToken, {path: '/'})
+            nookies.set(ctx, refreshToken, res.data.accessToken, {path: '/', secure: true, httpOnly: true})
 
             return res.data.accessToken
         } catch (e) {
