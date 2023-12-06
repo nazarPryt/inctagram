74a6f8b9884eb2a038e6acd26743569f6709d662
nazarPtyt <ockap90@libero.it>
2023-12-06 09:39:56 +0100

    feat: authorization on profile pages

--- src/pages/profile/favorites/index.tsx
+++ src/pages/profile/favorites/index.tsx
@@ -1,6 +1,27 @@
 import React from 'react'
 import {getAuthorizedLayout} from '_app/Layouts/authorized/AuthorizedLayout'
+import {GetServerSideProps, GetServerSidePropsContext} from 'next'
+import {serverAuthAPI} from 'shared/server-api/server-api'
+import {PATH} from 'shared/constants/PATH'
 
+export const getServerSideProps: GetServerSideProps = async (ctx: GetServerSidePropsContext) => {
+    const user = await serverAuthAPI.authMe(ctx)
+    console.log('serverAuthAPI.authMe (user): ', user)
+    if (user) {
+        return {
+            props: {
+                user,
+            },
+        }
+    } else {
+        return {
+            redirect: {
+                destination: PATH.LOGIN,
+                permanent: true,
+            },
+        }
+    }
+}
 export default function FavoritePage() {
     return <>Favorites page</>
 }
--- src/pages/profile/index.tsx
+++ src/pages/profile/index.tsx
@@ -1,7 +1,28 @@
 import React from 'react'
 import {getAuthorizedLayout} from '_app/Layouts/authorized/AuthorizedLayout'
 import {AllPostsList} from 'widgets/AllPostsList/AllPostsList'
+import {GetServerSideProps, GetServerSidePropsContext} from 'next'
+import {serverAuthAPI} from 'shared/server-api/server-api'
+import {PATH} from 'shared/constants/PATH'
 
+export const getServerSideProps: GetServerSideProps = async (ctx: GetServerSidePropsContext) => {
+    const user = await serverAuthAPI.authMe(ctx)
+    console.log('serverAuthAPI.authMe (user): ', user)
+    if (user) {
+        return {
+            props: {
+                user,
+            },
+        }
+    } else {
+        return {
+            redirect: {
+                destination: PATH.LOGIN,
+                permanent: true,
+            },
+        }
+    }
+}
 export default function Home() {
     return <AllPostsList posts={data.items} />
 }
