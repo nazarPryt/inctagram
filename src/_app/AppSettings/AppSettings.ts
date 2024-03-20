3800620536dbe47cb6f2ad4bf85063480b50f898
nazarPtyt <ockap90@libero.it>
2024-03-20 23:48:59 +0100

    seo: added correct image width/hight/description

--- src/_app/AppSettings/AppSettings.ts
+++ src/_app/AppSettings/AppSettings.ts
@@ -3,6 +3,7 @@ class AppSettings {
     public readonly BASE_URL: string
     public readonly COUNTRIES_URL: string
     public readonly DOMAIN_URL: string
+    public readonly OG_IMAGE_URL: string
 
     //keys
     public readonly RECAPTCHA_KEY: string
@@ -16,6 +17,7 @@ class AppSettings {
         this.BASE_URL = process.env.NEXT_PUBLIC_BASE_URL
         this.DOMAIN_URL = process.env.NEXT_PUBLIC_DOMAIN_URL
         this.COUNTRIES_URL = process.env.NEXT_PUBLIC_COUNTRIES_URL
+        this.OG_IMAGE_URL = 'https://maxwellinvestmentsgroup.com/wp-content/uploads/2020/07/IMG_1475.png' //todo find a way how to get it from public folder
 
         //keys
         this.RECAPTCHA_KEY = process.env.NEXT_PUBLIC_RECAPTCHA_KEY
--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -33,16 +33,22 @@ class MyDocument extends Document {
             <Html>
                 <Head>
                     <meta charSet={'utf-8'} />
+                    <meta content={'width=device-width, initial-scale=1'} name={'viewport'} />
                     <meta content={'The site for enthusiastic web developer'} name={'description'} />
                     <meta content={'Next, JS, Social network'} name={'keywords'} />
                     <meta content={'website'} property={'og:type'} />
-                    <meta content={'Inctareplica'} property={'og:title'} />
-                    <meta content={'This is the Open graph description'} property={'og:description'} />
-                    <meta content={appSettings.DOMAIN_URL} property={'og:url'} />
+                    <meta content={'Instareplica'} property={'og:title'} />
                     <meta
-                        content={'https://maxwellinvestmentsgroup.com/wp-content/uploads/2020/07/IMG_1475.png'}
-                        property={'og:image'}
+                        content={
+                            'Instareplica: A next-gen social media platform blending the best features of popular apps. Share photos, videos, and stories with interactive features like live streaming and polls. Prioritizes privacy and security while fostering authentic connections worldwide.'
+                        }
+                        property={'og:description'}
                     />
+                    <meta content={appSettings.DOMAIN_URL} property={'og:url'} />
+                    <meta content={appSettings.OG_IMAGE_URL} property={'og:image'} />
+                    <meta content={'750'} property={'og:image:width'} />
+                    <meta content={'372'} property={'og:image:height'} />
+                    <meta content={'Seriously!! just drop me a line :)'} property={'og:image:alt'} />
                     <Favicon />
                 </Head>
                 <body>
